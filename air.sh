6d5e8d6fad0a914092e0aedd7ca71165bcb2b3c1
k0k-1 <k@k0.gg>
2018-05-14 17:08:27 +0900

    first commit

---
+++ air.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+init(){ set -u;trap exit ERR }
+input(){ read -p "press [enter] to ${@:2}." KEY }
+chk_exist(){ local rslt=0;if ! type ${1} >/dev/null 2>&1; then result=1;fi;return ${rslt} }
+msg(){
+ 	local HEADER="|-- [${1}] "
+ 	case ${1} in
+ 		success)
+ 			local COLOR=47 # GREEN
+ 			local HEADER=${HEADERTYPE1}
+ 			local FOOTER=""
+ 			;;
+ 		failed)
+ 			local COLOR=197 #RED
+ 			local HEADER=${HEADERTYPE1}
+ 			local FOOTER=""
+ 			;;
+ 		msg)
+ 			local COLOR=216 #ORANGE
+ 			local HEADER="|-- [msg] "
+ 			local FOOTER=""
+ 			;;
+ 		*)
+ 			local COLOR=15 #WHITE
+ 			local HEADER=${HEADER}
+ 			local FOOTER=""
+ 			;;
+ 	esac
+ 	local HEADER="\x1b[38;05;${COLOR}m${HEADER}"
+ 	local FOOTER="${FOOTER}\033[00m\n"
+ 	printf "${HEADER}${@:2}${FOOTER}"
+}
+
+watch_start(){ SECONDS=0 }
+watch_end(){ echo "running time:${SECONDS} [s]" }
+
+# operator
+++(){ $(( ${1}=${1}+1 )) }
++=(){ $(( ${1}=${1}+${2} )) }
+--(){ $(( ${1}=${1}-1 )) }
+-=(){ $(( ${1}=${1}-${2} )) }
---
+++ readme.md
@@ -0,0 +1,8 @@
+# library for shell script
+
+Usage
+-------------------
+
+```bash
+<command> <option>
+```
