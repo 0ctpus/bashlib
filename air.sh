cc8d6cd540ca5d89c14d4312282ea52770f79126
k0k-1 <k@k0.gg>
2018-05-16 18:40:18 +0900

    change output of 'msg' function

--- air.sh
+++ air.sh
@@ -14,34 +14,72 @@ chk_exist(){
 	return ${rslt}
 }
 
-msg(){
-	local HEADER="|-- [${1}] "
-	case ${1} in
-		success)
-			local COLOR=47 # GREEN
-			local HEADER=${HEADERTYPE1}
-			local FOOTER=""
-			;;
-		failed)
-			local COLOR=197 #RED
-			local HEADER=${HEADERTYPE1}
-			local FOOTER=""
-			;;
-		msg)
-			local COLOR=216 #ORANGE
-			local HEADER="|-- [msg] "
-			local FOOTER=""
-			;;
-		*)
-			local COLOR=15 #WHITE
-			local HEADER=${HEADER}
-			local FOOTER=""
-			;;
-	esac
-	local HEADER="\x1b[38;05;${COLOR}m${HEADER}"
-	local FOOTER="${FOOTER}\033[00m\n"
-	printf "${HEADER}${@:2}${FOOTER}"
+# usage: msg {input|error|success|failed|em|msg} "message body"
+msg()
+{
+	local debug=0
+
+	if [ $1 = "input" ]; then
+		read -p "press [enter] to ${@:2}." KEY
+	else
+		case $1 in
+			error)
+				local fcolor=1   # red
+				local bcolor=239 # dark gray
+				local header="  ERROR  "
+				local footer="         "
+				;;
+			success)
+				local fcolor=240 # dark gray
+				local bcolor=40  # green
+				local header=" success "
+				local footer=""
+				;;
+			failed)
+				local fcolor=15  # white
+				local bcolor=161 # magenta
+				local header=" failed  "
+				local footer=""
+				;;
+			em)
+				local fcolor=33 # cyan
+				local bcolor=33 # cyan
+				local header="         "
+				local footer=""
+				;;
+			msg)
+				local fcolor=15  # white
+				local bcolor=239 # dark gray
+				local header="   msg   "
+				local footer=""
+				;;
+			dbg)
+				if [ ${debug} = 1 ];then
+					local fcolor=233 # dark gray
+					local bcolor=220 # yellow
+					local header="  debug  "
+					local footer=""
+				else
+					return
+				fi
+				;;
+			*)
+				local fcolor=15 # white
+				local bcolor=15 # white
+				local footer=""
+				;;
+		esac
+
+		local f="\x1b[38;05;${fcolor}m" # foreground color
+		local b="\x1b[48;05;${bcolor}m" # background color
+		local reset="\033[00m"
+		local header="${f}${b}${header}${reset} "
+		local footer=" ${f}${b}${footer}${reset}\n"
+
+		printf "${header}${@:2}${footer}"
+	fi
 }
+
 
 watch_start(){
 	SECONDS=0
